f78ee3378543ff0b2d36da31cbdd36353dc58bd8
May Bossink <90610796+may-bossink@users.noreply.github.com>
2024-10-04 15:18:02 +0200

    Add project files

---
+++ api-gateway/src/test/java/me/may/myfolio/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.may.myfolio.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ portfolio/src/test/java/me/may/myfolio/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.may.myfolio.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
